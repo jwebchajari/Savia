f7c49b9eb6a0c2dbcec1c9691a6551df18c117f2
jwebchajari <jwebchajari@gmail.com>
2025-11-18 17:03:34 -0300

    savia 3.0.0

--- src/_components/CategorySection/Allcategories.jsx
+++ src/_components/CategorySection/Allcategories.jsx
@@ -9,42 +9,42 @@ const categories = [
         description:
             "Ricos en fibra, antioxidantes y grasas saludables. Los frutos secos ayudan a mejorar la energía diaria, el corazón y la digestión.",
         img: "/category-banners/frutos-secos.png",
-        link: "/frutos-secos",
+        link: "categoria/frutos-secos",
     },
     {
         title: "Sin Lactosa",
         description:
             "Productos ligeros y fáciles de digerir. Ideales para quienes buscan bienestar digestivo sin dejar de disfrutar alimentos deliciosos.",
         img: "/category-banners/sin-lactosa.png",
-        link: "/sin-lactosa",
+        link: "categoria/sin-lactosa",
     },
     {
         title: "Veganos",
         description:
             "Una alimentación basada en plantas aporta nutrientes esenciales, energía limpia y beneficios para la salud.",
         img: "/category-banners/vegano.png",
-        link: "/vegano",
+        link: "categoria/vegano",
     },
     {
         title: "Sin TACC",
         description:
             "Opciones libres de gluten para quienes buscan cuidar su bienestar digestivo sin resignar sabor.",
         img: "/category-banners/sin-tacc.jpg",
-        link: "/sin-tacc",
+        link: "categoria/sin-tacc",
     },
     {
         title: "Sin Azúcar",
         description:
             "Productos sin azúcar añadida para quienes buscan reducir calorías o mantener una alimentación más saludable.",
         img: "/category-banners/sin-azucar.jpg",
-        link: "/sin-azucar",
+        link: "categoria/sin-azucar",
     },
     {
         title: "Yuyitos",
         description:
             "Infusiones naturales para relajarse, mejorar la digestión y equilibrar el bienestar.",
         img: "/category-banners/yuyitos.webp",
-        link: "/yuyitos",
+        link: "categoria/yuyitos",
     },
 ];
 
