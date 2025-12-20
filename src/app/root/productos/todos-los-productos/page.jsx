19404a7f9e61749038142e84fd202a7eeb54f3f2
jwebchajari <jwebchajari@gmail.com>
2025-12-19 23:05:13 -0300

    pruenba admins

--- src/app/root/productos/todos-los-productos/page.jsx
+++ src/app/root/productos/todos-los-productos/page.jsx
@@ -1,14 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
-import ProductosRootClient from "./ProductosRootClient";
-
-export default function Page() {
-    return (
-        <div className="container mt-5">
-            <ProductosRootClient />
-        </div>
-    );
+export default function ProductosRootClient() {
+    return <div>TEST ADMIN</div>;
 }
