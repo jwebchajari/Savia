6883a613c792f4b987c60c05536d80ecbbceafe0
jwebchajari <jwebchajari@gmail.com>
2025-12-19 23:19:28 -0300

    reinstla

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    externalDir: true,
-  },
-
-  srcDir: "src",
-
-  // 👇 Configuración para permitir imágenes externas
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-      }
+      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
+      { protocol: "https", hostname: "gstatic.com" },
+      { protocol: "https", hostname: "*.gstatic.com" },
+      { protocol: "https", hostname: "*.googleusercontent.com" }
     ],
   },
 };
